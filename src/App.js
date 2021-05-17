45e9c785bc2b4d0bc28d4dd23c5f9aa47697690e
Gabe N <nelsong1997@gmail.com>
2021-05-16 22:33:23 -0400

    auth link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
     }
 
     render() {
-        return <p>hello</p>
+        return <a href='https://discord.com/api/oauth2/authorize?client_id=843650184206483496&permissions=3072&redirect_uri=https%3A%2F%2Fnelsong1997.github.io%2Fgabebot%2F&scope=bot'>link</a>
     }
 }
 
